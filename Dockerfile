6b79f9bbb3f8e1073afab15ad68c4f6541885bb3
gregchalmers <g.chalmers@metocean.co.nz>
2017-06-21 11:41:56 +1200

    added adjustment of poll time

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM alpine
 RUN apk update
 RUN apk add rsync openssh
 ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/bin/sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 echo 'starting...'
+
+if [ $# -eq 1 ]; then
+  interval=$1
+elif [ ${POLLING_INTERVAL} ]; then
+  interval=${POLLING_INTERVAL}
+else
+  interval=30
+fi
+
+echo "polling interval: $interval seconds"
 while :
 do
   /usr/bin/rsync -av --delete /data_src/ /data_dest/
-  sleep 10
+  sleep $interval
 done
